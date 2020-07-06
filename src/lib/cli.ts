ebdddec5976eb4e68b1c2a2d880ae4c1b3407f83
Akshay Kumar <akshay.kmr4321@gmail.com>
2020-07-06 13:45:44 +0530

    preflight: checks version and auth

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,12 +1,50 @@
 
-import { config, env, setupENV } from './config';
+import chalk = require('chalk');
+const { Input } = require("enquirer");
+const ora = require('ora');
 
-export const preflightChecks = (env: env = config.get("env") || "prod") => {
+import { env, setupENV, getSuryaConfig, getENVAccessToken, setENVAccessToken } from './config';
+import { fetchCliManifest, initializeSurya, fetchSessionUser } from './surya';
+import { Unauthorized } from './surya/errors';
+
+const cliVersion = 0;
+
+
+const promptForToken = (): Promise<string> => {
+  return new Input({
+    name: "Access Token",
+    message:"Running for the first time? Please enter your access token for authentication",
+  })
+  .run()
+}
+
+export const preflightChecks = async (env: env) => {
+  const token = getENVAccessToken(env) || await promptForToken()
+  const spinner = ora('running preflight checks').start();
+  setENVAccessToken(env, token)
   setupENV(env)
+  const suryaConfig = getSuryaConfig();
+  initializeSurya(suryaConfig)
 
-  // version check
-
-  // token check or setup
-
-
+  fetchCliManifest()
+    .then((manifest) => {
+      if (manifest.cliVersion > cliVersion) {
+        spinner.stop(chalk.yellowBright("your oorja cli is outdated. please run: npm update -g oorja"))
+        process.exit(1)
+      }
+    })
+    .then(async () => {
+      const user = await fetchSessionUser()
+      console.log(user)
+    })
+    .catch((e) => {
+      if (e instanceof Unauthorized) {
+        spinner.fail("Your access token failed authentication, resetting...")
+        setENVAccessToken(env, '')
+      } else {
+        spinner.fail("something went wrong :(")
+        console.error(e)
+      }
+      process.exit(1)
+    })
 }
