94caedd385531f940b2d581a412c1194d7638494
Akshay Kumar <akshay.kmr4321@gmail.com>
2020-08-01 09:07:22 +0530

    ts-ignore axios patching

--- src/lib/surya/best_host.ts
+++ src/lib/surya/best_host.ts
@@ -4,29 +4,6 @@ const { performance } = require("perf_hooks");
 // poor mans anycast
 // ping and determine closest host on boot
 
-axios.interceptors.request.use((x) => {
-  x.meta = x.meta || {};
-  x.meta.requestStartedAt = performance.now();
-  return x;
-});
-
-axios.interceptors.response.use((x) => {
-  x.responseTime = performance.now() - x.config.meta.requestStartedAt;
-  return x;
-});
-
-const minPing = async (url: string, attempts = 2) => {
-  const responses: AxiosResponse[] = [];
-  for (let i = 0; i < attempts; i++) {
-    responses.push(
-      await axios.head(url, {
-        timeout: 2000,
-      })
-    );
-  }
-  return Math.min(...responses.map((r) => r.responseTime));
-};
-
 export const determineClosestHost = async (
   hosts: string[],
   tlsEnabled = true
@@ -43,4 +20,31 @@ export const determineClosestHost = async (
     if (!candidate.success) return current;
     return candidate.min < current.min ? candidate : current;
   }, candidates[0]).host;
+};
+
+axios.interceptors.request.use((x) => {
+  // @ts-ignore
+  x.meta = x.meta || {};
+  // @ts-ignore
+  x.meta.requestStartedAt = performance.now();
+  return x;
+});
+
+axios.interceptors.response.use((x) => {
+  // @ts-ignore
+  x.responseTime = performance.now() - x.config.meta.requestStartedAt;
+  return x;
+});
+
+const minPing = async (url: string, attempts = 2) => {
+  const responses: AxiosResponse[] = [];
+  for (let i = 0; i < attempts; i++) {
+    responses.push(
+      await axios.head(url, {
+        timeout: 2000,
+      })
+    );
+  }
+  // @ts-ignore
+  return Math.min(...responses.map((r) => r.responseTime));
 };
